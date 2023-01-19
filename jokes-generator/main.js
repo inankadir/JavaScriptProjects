46ecf639f99556a5648d65914cced79f8ef61d93
kadir <idia_2008@hotmail.com>
2023-01-19 19:37:31 +0300

    add new file

---
+++ jokes-generator/main.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "xIxN800ymTdti8VukRg7aA==KJ0JXPtUdTEQj7Gs";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
